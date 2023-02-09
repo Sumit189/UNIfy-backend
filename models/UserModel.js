8555a57c0e0918bdf98d02d0d3942c62e48f3eb5
Sumit Paul <sumit.18.paul@gmail.com>
2023-02-10 00:37:38 +0530

    update

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
-	userName: {type: String, required: true},
+	userName: {type: String, required: false},
 	category: {type: String, required: false},
 	feedbackId: {type: Number, required: false},
 	uuid: {type: String, required: true},
